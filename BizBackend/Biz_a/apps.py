814ed63c2882d2953b0f1ab870419565ed5deb55
ananya26-vishnoi <ananyavishnoi26@gmail.com>
2025-04-02 19:26:40 +0530

    add avtive

--- BizBackend/Biz_a/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BizAConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Biz_a'
--- BizBackend/Biz_a/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('getBusinessCards', views.get_business_cards, name='get_business_cards'),
-    path('createBusinessCard', views.create_business_card, name='create_business_card'),
-    path('updateBusinessCard', views.update_business_card, name='update_business_card'),
-    path('deleteBusinessCard', views.delete_business_card, name='delete_business_card'),
-]
