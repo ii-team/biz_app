802f2077cb5d0823b67171aee7fc37388f51e2d2
Ubuntu <ubuntu@ip-172-26-4-80.ca-central-1.compute.internal>
2025-04-08 16:48:05 +0000

    changes

--- BizBackend/Biz/settings.py
+++ BizBackend/Biz/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cr(bt3uou1)o(k$3w-s3km@u=((i6g=&=4$7i#ep=5p566yn16'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['biz-backend.famsketch.eu']
 CORS_ALLOW_ALL_ORIGINS = True
@@ -121,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL = "photos/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
